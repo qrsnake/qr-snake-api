7cd07f3d7c8bde582b7e00ff37ec557f6e533966
albertobazilio98 <albertobazilio98@gmail.com>
2021-08-27 20:02:13 -0400

    heroku port

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.info('listening on http://localhost:4000');
 
   router(app);
